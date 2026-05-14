f0da63613f7c0a0b2922ab1134f60de157bcd146
midkids <83714759+midkids@users.noreply.github.com>
2026-05-14 16:48:26 -0400

    Add ScrollView

--- Moonshot/ContentView.swift
+++ Moonshot/ContentView.swift
@@ -9,7 +9,9 @@
 import SwiftUI
 
 // Resizing images to fit the available space
+// How ScrollView lets us work with scrolling data
 
+/*
 struct ContentView: View {
     var body: some View {
     // When we create an image view in SwiftUI
@@ -50,6 +52,61 @@ struct ContentView: View {
             .containerRelativeFrame(.horizontal) {
                 size, axis in size * 0.8
             }
+    }
+}
+ */
+
+struct CustomText: View {
+    let text: String
+    
+    var body: some View {
+        Text(text)
+    }
+    
+    init(text: String) {
+        // This will show where in our code
+        // when SwiftUI makes this custom view
+        print("Creating a new CustomText")
+        self.text = text
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        // ScrollViews and scroll
+        // horizontally and vertically
+        // or in both directions at the same time
+        // Can show scroll indicator
+        // When we place views inside ScrollView,
+        // it automatically determines
+        // the size of its content
+        // It respects the safe area automatically
+        //   just like lists and forms
+        // In this case,
+        // it starts directly below the dynamic island
+        // When we add a view inside a ScrollView,
+        //   they are added immediately upon start up
+        //   if we use VStack
+        // To avoid that, we will change our VStack to
+        //   a lazy VStack which will only make the
+        //   views on the screen
+        ScrollView {
+            // VStack(spacing: 10) {
+            // A LazyVStack will always take up as much
+            //  space as available in our layouts
+            // Compared to a regular VStack only takes up
+            //   the space it actually needed
+            LazyVStack(spacing: 10) {
+                ForEach(0..<100) {
+                    CustomText(text: "Item \($0)")
+                        .font(.title)
+                }
+            }
+            // allows us to touch anywhere
+            // across the entire screen to scroll
+            // rather than just in the list area
+            .frame(maxWidth: .infinity)
+        }
     }
 }
 
